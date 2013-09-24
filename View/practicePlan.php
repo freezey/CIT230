d4b64531e391bc0c0d5b57a6dc0c949456da526c
Jordan Novak <freezey@gmail.com>
2013-09-24 16:12:15 -0500

    main practice plan

--- View/practicePlan.php
+++ View/practicePlan.php
@@ -1,1 +1,20 @@
-Welcome to my plan!
+<h1>Deliberate Practice Plan</h1>
+<h2>What is it?</h2>
+<p>The deliberate practice plan is a plan of action to organize practice time and effort in order to make it as effective as possible.</p>
+<h2>My Goals</h2>
+<ol>
+    <li>Enhance my design skills above the rudimentary</li>
+    <li>Learn new PHP practices that will push my programming skills even farther</li>
+</ol>
+<h2>Methods</h2>
+    <ul>
+        <li>Where: In my home office</li>
+        <li>When: Wednesday and Friday between 8am and 10am</li>
+        <li>Time: Minimum 2 hours per week. Goal: 4 hours per week</li>
+        <li>Who will judge my practice: Back-end tools will be shared with and critiqued by my team at work.  Design / creativity practice will be judged by my wife.</li>
+    </ul>
+
+<h2>Practice Session Reports</h2>
+<ul>
+    <li><a href="/Assessments/practice/2013-09-23">23 September 2013 - Building a PHP framework</a></li>
+</ul>
