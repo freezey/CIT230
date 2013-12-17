70fc5925ec332bf4b962a4dbe80ab514425f415d
Jordan Novak <freezey@gmail.com>
2013-12-16 20:02:22 -0600

    style guide write-out

---
+++ triathlon/View/styleGuide.php
@@ -0,0 +1,80 @@
+<h1>Jailbreak Triathlon Site Plan</h1>
+
+<h2>Purpose</h2>
+<p>
+Create public interest in the triathlon, Attract participants, and Divulge information necessary for visitors to attend or participate.
+</p>
+
+<h2>Audience</h2>
+<p>
+    Athletes and sports fans. Average age - 28 years old. Average income - 42k per year. These are people that enjoy a good challenge.
+</p>
+
+<h2>Pages</h2>
+<ul>
+    <li>Home
+        <ul>
+            <li>Map with images pop-out like</li>
+            <li>Cheesy advertisement line</li>
+            <li>Obvious link to registration</li>
+
+        </ul>
+    </li>
+    <li>Route
+        <ul>
+            <li>Running
+                <ul>
+                    <li>Image of San Quentin prison start-point</li>
+                    <li>Image of Wharf end-point</li>
+                    <li>Stated length of run</li>
+                    <li>Map of run</li>
+                    <li>Image of scenery alo</li>
+                </ul>
+            </li>
+            <li>Swimming
+                <ul>
+                    <li>Image of alcatraz from wharf</li>
+                    <li>Stated length of swim</li>
+                </ul>
+            </li>
+            <li>Biking
+            <ul>
+                <li>Map</li>
+                <li>Scenic pictures</li>
+                <li>Bike across the Golden Gate bridge, along the highway, on park roads, trails, and open fields next to the Bay</li>
+            </ul></li>
+        </ul>
+    </li>
+    <li>Registration Page
+    <ul>
+        <li>NOT REAL disclaimer</li>
+            <li>Contact form that sends email to address in hidden input field</li>
+            <li>Costs</li>
+        </li>
+    </ul></li>
+</ul>
+
+
+<h1>Jailbreak Traithlon Style Guide</h1>
+<h2>General Design</h2>
+<p><ul>
+    <li>Black background</li>
+    <li>Centered logo in header</li>
+    <li>Content on each page is to be contained in max-width:400px; max-height:350px divs that are semi-randomly scattered across the available page space.</li>
+    <li>Simple footer with necessary links</li>
+</ul>
+</p>
+<h2>Text</h2>
+<h3>Fonts</h3>
+<p><ul>
+    <li>Headers will use Permanent Marker (http://www.google.com/fonts#UsePlace:use/Collection:Permanent+Marker)</li>
+    <li>Regular text will be sans family</li>
+</ul>
+</p>
+<h3>Colors</h3>
+<p>
+    <ul>
+    <li>Text will be white (#FFFFFF)</li>
+    <li>Links will be an aqua green (#AAFFAA) and there will be no color change for :visited or :hover</li>
+    </ul>
+</p>
